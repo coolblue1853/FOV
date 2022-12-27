6df6791315a8dedab7408947e8e3597e3f62a518
coolblue1853 <54536107+coolblue1853@users.noreply.github.com>
2022-12-28 01:56:49 +0900

    fov 생성중

---
+++ Assets/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+public class Player : MonoBehaviour
+{
+    float angle;
+    Vector2 target, mouse;
+
+    [SerializeField] private myFieldOfView fieldOfView;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        angle = Mathf.Atan2(mouse.y - target.y, mouse.x - target.x) * Mathf.Rad2Deg;
+        this.transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+
+
+
+        Vector3 targetposition = UtilsClass.GetMouseWorldPosition();
+        Vector3 aimDir = (targetposition - transform.position).normalized;
+
+
+        fieldOfView.SetAimDir(aimDir);
+        fieldOfView.setOrigin(transform.position);
+    }
+}
