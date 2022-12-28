4b7b1f2fc054a7d08b5811f854c8a37e55496a79
coolblue1853 <54536107+coolblue1853@users.noreply.github.com>
2022-12-28 11:08:51 +0900

    FOV 시스템 완성

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using CodeMonkey.Utils;
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float Speed;
     float angle;
     Vector2 target, mouse;
 
@@ -17,10 +18,11 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
         mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         angle = Mathf.Atan2(mouse.y - target.y, mouse.x - target.x) * Mathf.Rad2Deg;
         this.transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
-
+        */
 
 
         Vector3 targetposition = UtilsClass.GetMouseWorldPosition();
@@ -29,5 +31,12 @@ public class Player : MonoBehaviour
 
         fieldOfView.SetAimDir(aimDir);
         fieldOfView.setOrigin(transform.position);
+
+
+        // 이동
+
+        float X = Input.GetAxisRaw("Horizontal");
+        float Y = Input.GetAxisRaw("Vertical");
+        transform.Translate(new Vector2(X, Y) * Time.deltaTime * Speed);
     }
 }
